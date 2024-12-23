6d0f6417184f66bb0f9af4ff46a6c43bbd359252
EmaMSanchez <emamsanchez07@gmail.com>
2024-12-23 20:30:15 -0300

    Number

--- src/components/BotonBuy.jsx
+++ src/components/BotonBuy.jsx
@@ -26,7 +26,7 @@ const BotonBuy = () => {
            });
           
            redirigirAWhatsApp(productos, precioTotal)
-           reiniciarLocal()
+           reiniciarLocal();
         }
 
   return (
