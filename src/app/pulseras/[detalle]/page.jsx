9df5a0c647bd0dfffcc7a9d01259e7e00fc4f965
EmaMSanchez <emamsanchez07@gmail.com>
2024-12-24 16:06:20 -0300

    V1.0

--- src/app/pulseras/[detalle]/page.jsx
+++ src/app/pulseras/[detalle]/page.jsx
@@ -26,23 +26,23 @@ const DetallPulsera = () => {
             />
             </div>
            
-            <div className="container flex items-center justify-center md:mr-0 ">
-            <div className="w-[85%] h-[86%] md:w-[100%] md:h-[87.5%] md:max-w-xs bg-white border border-gray-200 rounded-lg shadow">
-            <img className="w-[82%] ml-5 mt-[-20px] md:mt-0 md:ml-0 md:w-80 md:p-6 rounded-t-lg" src="/logoNav.png" alt="product image" />
-        <div className="px-5 md:pb-5 md:mt-[-40px]">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 mt-[-40px] md:mt-0">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-               <input  type="number"
-                    min="1"
-                    value={cantidad}
-                    onChange={(e) => setCantidad(Number(e.target.value))}
-                    className="w-16 mt-2 mb-8 md:my-4  text-center border rounded-md"/>
-            <div className="flex items-center justify-between">
-                <span className="text-2xl font-bold text-gray-900 ">${datos.precio}</span>
-              <ButtonAddProduct disabled={cantidad < 1} datos={datos} cantidad={cantidad} resetCantidad={() => setCantidad(1)}/>
-            </div>
+            <div className="container flex items-center justify-center mr-1 md:mr-0 ">
+        <div className="w-[85%] h-[86%] md:w-[100%] md:h-[87.5%] md:max-w-xs bg-white border border-gray-200 rounded-lg shadow">
+        <img className="w-[82%] ml-5 mt-[-20px] md:mt-0 md:ml-0 md:w-80 md:p-6 rounded-t-lg" src="/logoNav.png" alt="product image" />
+    <div className="px-5 md:pb-5 md:mt-[-40px]">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 mt-[-40px] md:mt-0">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+           <input  type="number"
+                min="1"
+                value={cantidad}
+                onChange={(e) => setCantidad(Number(e.target.value))}
+                className="w-16 mt-2 mb-10 md:my-4  text-center border rounded-md"/>
+        <div className="flex items-center justify-between mb-2 md:mb-0">
+            <span className="text-2xl font-bold text-gray-900 ">${datos.precio}</span>
+          <ButtonAddProduct disabled={cantidad < 1} datos={datos} cantidad={cantidad} resetCantidad={() => setCantidad(1)}/>
         </div>
     </div>
-            </div>
+</div>
+        </div>
             </div>
           );
         }
