2154b7c93a92c49f2f0fa676e3dc6cf29a927947
EmaMSanchez <emamsanchez07@gmail.com>
2024-12-31 14:13:20 -0300

    Test2.0

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { DATOCARDSINIT } from '@/mock/datoInit'
 export default function Home() {
 
   return (
-    <div className="container flex flex-col justify-center items-center md:ml-12 2xl:ml-24 mt-3 ">
+    <div className="container flex flex-col justify-center items-center md:ml-12 2xl:ml-48 mt-3 ">
      <Carrousel/>
     <div className='m-7'>
     <h4 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-[#966720a4] from-[#f3dcb9a4]">Inicio</h4>
